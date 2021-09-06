f6b9c9d9531c4406faa9ef209f56295df98d6bb4
Mike-GB <88850952+Mike-GB@users.noreply.github.com>
2021-09-06 18:15:16 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,9 +1,9 @@
 
 
 def main():
-    #Escribe tu código debajo de esta línea
-    
-
+   
+    num = int(input("Escribe un numero : "))
+    #escribe tu código abajo de esta línea
     v=1
     while v**2<=num:
         v=v+1
